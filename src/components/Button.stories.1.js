761f481a4c10e03d4abbdc88780988b233544790
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-04-29 12:01:03 +0200

    Remove unused button story.

--- src/components/Button.stories.1.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Button from './Button';
-
-storiesOf('Button', module).add('default', () => <Button>Click me</Button>);
