f2a4e382f7d6d73286886dc69de8c3498941c4d6
Thomas Mayrhofer <thomas@mayrhofer.at>
2021-03-01 15:11:10 +0100

    code from lecture

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -4,8 +4,11 @@ import { action } from '@storybook/addon-actions'
 import Button from './Button'
 
 storiesOf('Button', module)
-  .add('primary (default)', () => (
-    <Button onClick={action('clicked')}>Click me</Button>
+  .add('primary disabled true', () => (
+    <Button disabled={true}>Click me</Button>
+  ))
+  .add('primary disabled false', () => (
+    <Button disabled={false}>Click me</Button>
   ))
   .add('primary disabled', () => (
     <Button onClick={action('clicked')} disabled>
