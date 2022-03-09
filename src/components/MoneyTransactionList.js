5bd462791ab392b7de3cd1a68b5deae1b0423c8c
Juliane Mohr <julianemohr@hotmail.com>
2022-03-09 11:43:16 +0100

    Add Stylings and props to TableRow

--- src/components/MoneyTransactionList.js
+++ src/components/MoneyTransactionList.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { TableRow } from './TableRow'
 
 export const MoneyTransactionList = () => {
+  const list = [
+    {
+      user: {
+        id: 1,
+        name: 'Juliane'
+      },
+      amount: 10.4,
+      isPaid: true
+    },
+    {
+      user: {
+        id: 2,
+        name: 'Claudia'
+      },
+      amount: 10.4,
+      isPaid: false
+    }
+
+  ]
   return (
         <>
+            {list.map((element) => {
+              return (
+                    <TableRow key={element.user.id} userName={element.user.name} id={element.user.id} isPaid={element.isPaid} amount={element.amount}/>
+              )
+            })}
         </>
   )
 }
--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import styles from './TableRow.module.css'
+import { Button } from './Button'
 
-export const TableRow = ({ userName }) => {
+export const TableRow = ({ userName, isPaid, id, amount, setPaid }) => {
+  const formatAmount = (num) => {
+    return `${num.toFixed(2)}$`
+  }
+
   return (
-        <div className={styles.rowWrapper}>
+        <div className={styles.rowWrapper} id={id}>
             <p className={styles.lableText}>{userName}</p>
+            <div className={styles.amountWrapper}>
+                {isPaid
+                  ? <p className={`${styles.lineThrough} ${styles.lableText}`}>{formatAmount(amount)}</p>
+                  : <>
+                            <p className={styles.lableText}>{formatAmount(amount)}</p>
+                            <Button isPrimary={true} onClick={setPaid}>Paid</Button>
+                        </>
+                }
+            </div>
         </div>
   )
 }
