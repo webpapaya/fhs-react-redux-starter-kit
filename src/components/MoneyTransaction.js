bba5e8dbdf36e1ebf21d4b4a14def72d89813a77
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-03-12 21:39:09 +0100

    restyling components to match styleguide

--- src/components/MoneyTransaction.js
+++ src/components/MoneyTransaction.js
@@ -7,8 +7,8 @@ export const MoneyTransaction = ({ user, amount, paid = false }) => {
     <>
       <td className={`${paid ? `${styles.paid} ` : ''}`}>{user}</td>
       <td className={`${paid ? `${styles.paid}` : ''} ${styles.alignend}`}>{amount}</td>
-      { !paid && <td className={`${styles.alignend} ${styles.button}`}><Button onClick="Paid">Paid</Button></td> }
-      { paid && <td></td> }
+      {!paid && <td className={`${styles.alignend} ${styles.button}`}><Button onClick="Paid">Paid</Button></td>}
+      {paid && <td></td>}
     </>
   )
 }
--- src/components/MoneyTransactionCreate.js
+++ src/components/MoneyTransactionCreate.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Button } from './Button'
 import { SelectInput } from './SelectInput'
-import { DecimalInput } from './DecimalInput'
+import { TextInput } from './TextInput'
 import styles from './MoneyTransactionCreate.module.css'
 
 export const MoneyTransactionCreate = () => {
   return (
     <form className={`${styles.transaction}`}>
       <SelectInput label='User' id='user'></SelectInput>
-      <DecimalInput label='Amount' id='amount'></DecimalInput>
+      <TextInput type="text" id="amount" label="Amount"></TextInput>
       <Button onClick="Create Transaction">Create</Button>
     </form>
   )
