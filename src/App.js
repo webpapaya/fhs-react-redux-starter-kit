848b5116da467a40cb9154bc024d46e7eb60a5b6
Thomas Mayrhofer <thomas.mayrhofer@sclable.com>
2022-03-14 13:53:39 +0100

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { useState } from 'react'
 import { useFormik } from 'formik'
 import { object, string } from 'yup'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 const Navigation = ({ user }) => {
-  return (<nav>{user.name}</nav>)
+  return (
+    <nav>
+      Navigation: {user.name}
+      <Link to='/'>Home</Link>
+      <Link to='/user'>User</Link>
+    </nav>
+  )
 }
 
 const App = () => {
-  const [user, setUser] = useState({ name: 's' })
+  const [user, setUser] = useState({ name: 'test' })
 
   return (
-    <div>
-        <Navigation user={user} />
-        <UserForm user={user} onUpdateUser={setUser} />
-    </div>
+    <Router>
+      <Navigation user={user} />
+      <Routes>
+        <Route
+          path='/user'
+          element={<UserForm user={user} onUpdateUser={setUser} />}
+        />
+        <Route path='/' element={<div>Home</div>} />
+      </Routes>
+    </Router>
   )
 }
 
