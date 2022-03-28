596a90fc6d720d62729e34c1d5665792f371f62a
Thomas Mayrhofer <thomas.mayrhofer@sclable.com>
2022-03-28 17:47:52 +0200

    feat: add redux action creator

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Button.module.css'
 
+const useHTTPEndpoint = input => {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetch(input)
+      .then(response => response.json())
+      .then(json => setTodos(json))
+  }, [])
+
+  return todos
+}
+
+export const FetchCustomHook = ({ onClick, children }) => {
+  const todos = useHTTPEndpoint('https://jsonplaceholder.typicode.com/todos')
+
+  console.log(todos)
+
+  return null
+}
+
 export const Button = ({ onClick, children }) => {
-  console.log(onClick)
+  const [counter, setCounter] = useState(1)
+
+  useEffect(() => {
+    // fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+
+    const onScroll = (evt) => console.log('eventlistener', counter)
+    document.addEventListener('click', onScroll)
+
+    return () => {
+      document.removeEventListener('click', onScroll)
+    }
+  })
+
+  console.log('state', counter)
+
   return (
-  <button onClick={onClick} className={`${styles.button} ${styles.primary}`}>
+  <button onClick={() => setCounter(counter + 1)} className={`${styles.button} ${styles.primary}`}>
     {children}
   </button>
   )
