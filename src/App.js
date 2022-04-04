15b5d60eaac0cbdf998252dd12cf011c7eb7aa0d
Thomas Mayrhofer <thomas.mayrhofer@sclable.com>
2022-04-04 13:11:28 +0200

    feat: showcase React.memo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function App () {
-  return (
-    <div>
-        A React Application
-    </div>
-  )
+
+const UserList = React.memo(({users}) => {
+    return (
+        <ul>
+            {users.map((user) => {
+                return <li>{user}</li>
+            })}
+        </ul>
+    )
+})
+
+function App() {
+    const [state, setState] = useState({ users: ['a', 'b', 'c'] })
+    const onUpdateUser = () => {
+        // Don't push to state.users
+        state.users.push('d')
+
+        // Do spread users and create new array
+        setState({ ...state, users: [...state.users, 'd'] })
+    }
+
+    return (
+        <div>
+            <UserList users={state.users} />
+            <button onClick={onUpdateUser}>Test</button>
+        </div>
+    )
 }
 
 export default App
